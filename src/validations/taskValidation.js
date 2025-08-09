da326c6d204e932009c683658607c836989145cc
Shivakumar1310 <srajendran@4labsinc.com>
2025-08-09 13:15:58 +0530

    new changes

--- src/validations/taskValidation.js
+++ src/validations/taskValidation.js
@@ -2,15 +2,15 @@
 import Joi from "joi";
 
 export const createTaskValidation = Joi.object({
-  title: Joi.string().min(10).required(),
-  description: Joi.string().min(50).required(),
+  title: Joi.string().min(3).required(),
+  description: Joi.string().min(10).required(),
   status: Joi.string().valid("pending", "in-progress", "completed").optional(),
   dueDate: Joi.date().required(),
 });
 
 export const updateTaskValidation = Joi.object({
-  title: Joi.string().min(10).optional(),
-  description: Joi.string().min(50).optional(),
+  title: Joi.string().min(3).optional(),
+  description: Joi.string().min(10).optional(),
   status: Joi.string().valid("pending", "in-progress", "completed").optional(),
   dueDate: Joi.date().optional(),
 });
